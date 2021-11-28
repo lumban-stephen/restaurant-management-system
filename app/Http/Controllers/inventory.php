79a761b5b07affbb9944e41264282506d13aad40
stpn-lmbn <stephenlumban@gmail.com>
2021-11-28 14:07:16 +0800

    Update inventory.php
    Added functions in controller

--- app/Http/Controllers/inventory.php
+++ app/Http/Controllers/inventory.php
@@ -1,10 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\InventoryModel;
 use Illuminate\Http\Request;
 
 class inventory extends Controller
 {
     //
+
+    public function index() {
+        $data = InventoryModel::all(); //Retrieves all data from the inventory model
+        return $data; //return statement
+    }
+
+    public function create() {
+        
+    }
+
+    public function store(Request $request) {
+        $data = new InventoryModel();
+        $data->Ingredient = $request->Ingredient;
+    }
+
+    public function show($id) {
+
+    }
+
+    public function edit($id) {
+        $data = InventoryModel::find($id);
+        return $data;
+    }
+
+    public function update(Request $request, $id) {
+
+    }
+
+    public function destroy($id) {
+        $data = InventoryModel::find($id);
+        $data->delete();
+    }
 }
