98f481c380c7bd00e57ca1f801614b2be1c59e09
stpn-lmbn <stephenlumban@gmail.com>
2021-12-01 18:20:31 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -14,4 +14,22 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+    <script src="{{asset('js/app.js')}}"></script>
+</x-app-layout> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div id="example">
+        
+    </div>
+    
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
