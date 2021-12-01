1ee55d7480a337cde7f15842aae3fd1e03ccd69e
stpn-lmbn <stephenlumban@gmail.com>
2021-12-01 18:15:53 +0800

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,12 @@ Route::get('/', function () {
     return view('auth/login');
 });
 
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
