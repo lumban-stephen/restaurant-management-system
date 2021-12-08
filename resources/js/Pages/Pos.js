7d56fb25350ec40034638e096c642896f47a5f35
stpn-lmbn <stephenlumban@gmail.com>
2021-12-08 17:28:53 +0800

    Update Pos.js

--- resources/js/Pages/Pos.js
+++ resources/js/Pages/Pos.js
@@ -15,7 +15,7 @@ export default function Pos(props) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">
-                                
+                            Hello World!
                         </div>
                     </div>
                 </div>
