7d4574d58e73e02edfae4b771176fcb143065c6c
stpn-lmbn <stephenlumban@gmail.com>
2021-12-08 17:19:23 +0800

    Update Inventory.js
    naming conventions

--- resources/js/Pages/Inventory.js
+++ resources/js/Pages/Inventory.js
@@ -3,14 +3,14 @@ import Authenticated from '@/Layouts/Authenticated';
 import { Head } from '@inertiajs/inertia-react';
 import Ingredients from './components/Ingredients';
 
-export default function EmployeeManagement(props) {
+export default function Inventory(props) {
     return (
         <Authenticated
             auth={props.auth}
             errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">EmployeeManagement</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Inventory</h2>}
         >
-            <Head title="EmployeeManagement" />
+            <Head title="Inventory" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
