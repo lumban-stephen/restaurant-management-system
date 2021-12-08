dec3300f7d61829f75e9e6c32f9a6e58d353e54a
stpn-lmbn <stephenlumban@gmail.com>
2021-12-08 19:26:32 +0800

    Create DishIngredients.php
    Pivot table for dish and ingredients

---
+++ app/Models/DishIngredients.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DishIngredients extends Model
+{
+    use HasFactory;
+}
