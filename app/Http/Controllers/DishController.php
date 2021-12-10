f45b2d9ce76dfd21a3cc4b841b38091c32539185
stpn-lmbn <stephenlumban@gmail.com>
2021-12-10 23:46:59 +0800

    Update DishController.php

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -13,4 +13,31 @@ class DishController extends Controller
 
         return $dishes;
     }
+
+    public function dishAdd( Request $request)
+    {
+        $dish = $request->input('dish_name');
+        $price = $request->input('price');
+
+        DB::update("INSERT INTO `DISHES` (dish_name, price) VALUES ($dish, $price)",[$dish,$price,auth()->user()->id]);
+        return redirect()->back()->with('status','Dish Added Successfully');
+    }
+
+    public function dishUpdate( Request $request)
+    {
+        $dish = $request->input('dish_name');
+        $price = $request->input('price');
+
+        DB::update("UPDATE `DISHES` SET dish_name = $dish, price = $price WHERE id = $request->input('id');",[$dish,$price,auth()->user()->id]);
+        return redirect()->back()->with('status','Dish Updated Successfully');
+    }
+
+    public function dishDelete( Request $request)
+    {
+        $dish = $request->input('dish_name');
+        $price = $request->input('price');
+
+        DB::update("DELETE FROM `DISHES` WHERE id = $request->input('id');",[$dish,$price,auth()->user()->id]);
+        return redirect()->back()->with('status','Dish Added Successfully');
+    }
 }
